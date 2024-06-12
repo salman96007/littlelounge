5d93fbff78e5e876c1efedf9585eeb7b7abd7e39
musthafa <musthafakm003@gmail.com>
2024-06-12 12:37:03 +0530

    color added

---
+++ lib/constant/colorconstant.dart
@@ -0,0 +1,16 @@
+
+
+
+
+import 'dart:ui';
+
+class ColorConst{
+  static const primaryColor=Color(0xffFFFFFF);
+  static const secondary=Color(0xffD1E20);
+  static const thirdColor=Color(0xff9775FA);
+  static const forth=Color(0xffEE65DB);
+
+
+
+
+}
