425b19edb02aeaa4f7cc4082505cc2d6d3256e51
fasil <fasilrahman96@gmail.com>
2024-06-13 09:55:12 +0530

    all folders are cleared

--- lib/constant/colorconstant.dart
+++ lib/constant/colorconstant.dart
@@ -1,12 +1,9 @@
 
-
-
-
 import 'dart:ui';
 
 class ColorConst{
   static const primaryColor=Color(0xffFFFFFF);
-  static const secondary=Color(0xffD1E20);
+  static const secondary=Color(0xff000000);
   static const thirdColor=Color(0xff9775FA);
   static const forth=Color(0xffEE65DB);
 
