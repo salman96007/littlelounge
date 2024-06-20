9f67dbb6da840a5f3f2ee05bfcd4ea59675ffc1f
salman <salmanmonusalman66@gmail.com>
2024-06-20 11:47:43 +0530

    welcome page complete

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:littlelounge/features/auth/screen/createaccount.dart';
+import 'package:littlelounge/features/auth/screen/loginpage.dart';
+import 'package:littlelounge/features/auth/screen/signuppage.dart';
 import 'package:littlelounge/features/auth/screen/verificationcode.dart';
 import 'package:littlelounge/features/home/screen/homepage.dart';
-import 'package:littlelounge/features/payement/screen/continueshopping.dart';
+import 'package:littlelounge/features/home/screen/welcomepage.dart';
 import 'package:littlelounge/features/payement/screen/saveaddresspage.dart';
+import 'package:littlelounge/features/revviews/screen/addreview.dart';
 import 'package:littlelounge/features/splashscreen/screen/splashscreen.dart';
 
  var height;
@@ -35,7 +38,8 @@ class _MyAppState extends State<MyApp> {
         theme: ThemeData(
             textTheme: GoogleFonts.interTextTheme()
         ),
-        home:Continueshopping(),
+
+        home:WelcomPage(),
         debugShowCheckedModeBanner: false,
       ),
     );
