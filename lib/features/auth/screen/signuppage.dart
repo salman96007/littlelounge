eb2ca0b8c34bb73b6e91575dadd7f3d2dfe60b0f
salman <salmanmonusalman66@gmail.com>
2024-06-27 11:07:13 +0530

    validator

--- lib/features/auth/screen/signuppage.dart
+++ lib/features/auth/screen/signuppage.dart
@@ -18,6 +18,10 @@ class Signup extends ConsumerStatefulWidget {
 }
 
 class _SignupState extends ConsumerState<Signup> {
+  final emailvalidation=RegExp(r"^[a-z-0-9.a-z-0-9.!#$%&'*+-/=?^_`{|}~]+@[a-z-0-9]+\.[a-z-]+");
+  final passwordvalidation= RegExp(r'^(?=.?[A-Z])(?=.?[a-z])(?=.?[0-9])(?=.?[!@#\$&*~]).{8,}$');
+  final formkey= GlobalKey<FormState>();
+
 
   add(){
 
@@ -53,143 +57,154 @@ class _SignupState extends ConsumerState<Signup> {
 
             Container(
               height: height*0.45,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  TextFormField(
-                    controller: usernameController,
-                    keyboardType: TextInputType.name,
-                    textInputAction:TextInputAction.next,
-                    decoration: InputDecoration(
-                      labelText:"Username",
-                      suffixIcon: SvgPicture.asset(SvgConstant.greenTick,fit: BoxFit.none,),
-                      labelStyle:TextStyle(
-                          color: ColorConst.secondary,
-                          fontWeight: FontWeight.w400
+              child: Form(
+                key: formkey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    TextFormField(
+                      controller: usernameController,
+                      keyboardType: TextInputType.name,
+                      textInputAction:TextInputAction.next,
+                      decoration: InputDecoration(
+                        labelText:"Username",
+                        suffixIcon: SvgPicture.asset(SvgConstant.greenTick,fit: BoxFit.none,),
+                        labelStyle:TextStyle(
+                            color: ColorConst.secondary,
+                            fontWeight: FontWeight.w400
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(width*0.03),
+                          borderSide: BorderSide(color: ColorConst.secondary),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(width*0.03),
+                          borderSide: BorderSide(color: ColorConst.secondary),
+                        ),
+
+
                       ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(width*0.03),
-                        borderSide: BorderSide(color: ColorConst.secondary),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(width*0.03),
-                        borderSide: BorderSide(color: ColorConst.secondary),
-                      ),
+
 
 
                     ),
+                    TextFormField(
+                      controller: passwordController,
+                      keyboardType: TextInputType.visiblePassword,
+                      textInputAction:TextInputAction.next,
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: (value) {
+                        if(!passwordvalidation.hasMatch(value!)){
+                          return "Incorect Password";
+                        }else{
+                          return null;
+                        }
+                      },
+                      decoration: InputDecoration(
+                        labelText:"Password",
+                        suffixIcon: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Strong",style: TextStyle(
+                              fontSize: width*0.03,
+                              color:ColorConst.eightethColor ,
+                            ),),
+                          ],
+                        ),
+                        labelStyle:TextStyle(
+                            color: ColorConst.secondary,
+                            fontWeight: FontWeight.w400
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(width*0.03),
+                          borderSide: BorderSide(color: ColorConst.secondary),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(width*0.03),
+                          borderSide: BorderSide(color: ColorConst.secondary),
+                        ),
 
 
+                      ),
 
-                  ),
-                  TextFormField(
-                    controller: passwordController,
-                    keyboardType: TextInputType.visiblePassword,
-                    textInputAction:TextInputAction.next,
-                    decoration: InputDecoration(
-                      labelText:"Password",
-                      suffixIcon: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text("Strong",style: TextStyle(
-                            fontSize: width*0.03,
-                            color:ColorConst.eightethColor ,
-                          ),),
-                        ],
-                      ),
-                      labelStyle:TextStyle(
-                          color: ColorConst.secondary,
-                          fontWeight: FontWeight.w400
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(width*0.03),
-                        borderSide: BorderSide(color: ColorConst.secondary),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(width*0.03),
-                        borderSide: BorderSide(color: ColorConst.secondary),
-                      ),
 
 
                     ),
+                    TextFormField(
+                      controller: emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      textInputAction:TextInputAction.next,
+                      decoration: InputDecoration(
+                        labelText:"Email Addresse",
+                        suffixIcon: SvgPicture.asset(SvgConstant.greenTick,fit: BoxFit.none,),
+                        labelStyle:TextStyle(
+                            color: ColorConst.secondary,
+                            fontWeight: FontWeight.w400,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(width*0.03),
+                          borderSide: BorderSide(color: ColorConst.secondary),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(width*0.03),
+                          borderSide: BorderSide(color: ColorConst.secondary),
+                        ),
 
 
+                      ),
 
-                  ),
-                  TextFormField(
-                    controller: emailController,
-                    keyboardType: TextInputType.emailAddress,
-                    textInputAction:TextInputAction.next,
-                    decoration: InputDecoration(
-                      labelText:"Email Addresse",
-                      suffixIcon: SvgPicture.asset(SvgConstant.greenTick,fit: BoxFit.none,),
-                      labelStyle:TextStyle(
-                          color: ColorConst.secondary,
-                          fontWeight: FontWeight.w400
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(width*0.03),
-                        borderSide: BorderSide(color: ColorConst.secondary),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(width*0.03),
-                        borderSide: BorderSide(color: ColorConst.secondary),
-                      ),
 
 
                     ),
-
-
-
-                  ),
-                  Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text("Remember me"),
-                      Stack(
-                        children: [
-                          InkWell(
-                            onTap: () {
-                              toggle = !toggle;
-                              setState(() {});
-                            },
-                            child: Container(
-                              height: width * 0.08,
-                              width: width * 0.15,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(width * 0.09),
-                                color: toggle == true ? ColorConst.seventeenColor:ColorConst.fourtyColor,
+                    Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Remember me"),
+                        Stack(
+                          children: [
+                            InkWell(
+                              onTap: () {
+                                toggle = !toggle;
+                                setState(() {});
+                              },
+                              child: Container(
+                                height: width * 0.08,
+                                width: width * 0.15,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(width * 0.09),
+                                  color: toggle == true ? ColorConst.seventeenColor:ColorConst.fourtyColor,
+                                ),
                               ),
                             ),
-                          ),
 
-                          AnimatedPositioned(
-                              top: width*0.005,
+                            AnimatedPositioned(
+                                top: width*0.005,
 
-                              left: toggle == true ? width*0.07 :width*0.01,
-                              child: InkWell(
-                                onTap: () {
-                                  toggle = !toggle;
-                                  setState(() {});
-                                },
-                                child: AnimatedContainer(
-                                    width: width*0.07,
-                                    height: width*0.07,
+                                left: toggle == true ? width*0.07 :width*0.01,
+                                child: InkWell(
+                                  onTap: () {
+                                    toggle = !toggle;
+                                    setState(() {});
+                                  },
+                                  child: AnimatedContainer(
+                                      width: width*0.07,
+                                      height: width*0.07,
 
-                                    decoration: BoxDecoration(
-                                        shape: BoxShape.circle,
-                                        color:  toggle == true ? ColorConst.primaryColor:ColorConst.primaryColor),
-                                    duration: Duration(
-                                      milliseconds: 500,
-                                    )),
-                              ),
-                              duration: const Duration(milliseconds: 200)),
-                        ],
-                      ),
+                                      decoration: BoxDecoration(
+                                          shape: BoxShape.circle,
+                                          color:  toggle == true ? ColorConst.primaryColor:ColorConst.primaryColor),
+                                      duration: Duration(
+                                        milliseconds: 500,
+                                      )),
+                                ),
+                                duration: const Duration(milliseconds: 200)),
+                          ],
+                        ),
 
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
 
@@ -198,6 +213,13 @@ class _SignupState extends ConsumerState<Signup> {
               children: [
                 InkWell(
                   onTap: () {
+                    if(usernameController.text !="" &&
+                      passwordController.text != "" &&
+                      emailController.text != ""&&
+                    formkey.currentState!.validate()
+                    ){
+                      add();
+                    }
                     add();
                   },
                   child: Container(
