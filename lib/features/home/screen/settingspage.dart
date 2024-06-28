f00479670a10f53cccf86fec5619ebfe64e3a7ec
jibinthomas <jibinn81@gmail.com>
2024-06-28 11:15:49 +0530

    page added

--- lib/features/home/screen/settingspage.dart
+++ lib/features/home/screen/settingspage.dart
@@ -18,11 +18,105 @@ class _SettingsPageState extends State<SettingsPage> {
         elevation: 1,
         title: Text("Settings",style: TextStyle(color: ColorConst.secondary,fontWeight: FontWeight.w600),),
       ),
-      body: Column(
-        children: [
-          Text("MY ACCOUNT",style: TextStyle(color: ColorConst.secondary,fontWeight: FontWeight.w600,fontSize: width*0.04),),
-
-        ],
+      body: Padding(
+        padding:  EdgeInsets.all(width*0.05),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Column(
+               children: [
+                Text("My Account",style: TextStyle(color: ColorConst.secondary,fontWeight: FontWeight.w600,fontSize: width*0.04),),
+              ],
+            ),
+            Column(
+              children: [
+                Container(
+                  height: height*0.05,
+                  width: width*0.95,
+                  child: Row(
+                    children: [
+                      Text("Edit profile",style: TextStyle(color: ColorConst.primaryColor,fontSize: width*0.05,fontWeight: FontWeight.w600,),),
+                      Icon(Icons.arrow_forward_ios_rounded,color: ColorConst.primaryColor,),
+                      SizedBox(),
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                      color: ColorConst.seventh,
+                      borderRadius: BorderRadius.circular(width*0.03)
+                  ),
+                ),
+                Container(
+                  height: height*0.05,
+                  width: width*0.95,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Change password",style: TextStyle(color: ColorConst.primaryColor,fontSize: width*0.05,fontWeight: FontWeight.w600,),),
+                      Icon(Icons.arrow_forward_ios_rounded,color: ColorConst.primaryColor,)
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                      color: ColorConst.seventh,
+                      borderRadius: BorderRadius.circular(width*0.03)
+                  ),
+                ),
+                Container(
+                  height: height*0.05,
+                  width: width*0.95,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Phone number",style: TextStyle(color: ColorConst.primaryColor,fontSize: width*0.05,fontWeight: FontWeight.w600,),),
+                      Icon(Icons.arrow_forward_ios_rounded,color: ColorConst.primaryColor,)
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                      color: ColorConst.seventh,
+                      borderRadius: BorderRadius.circular(width*0.03)
+                  ),
+                ),
+              ],
+            ),
+            Divider(),
+            Column(children: [
+              Text("More",style: TextStyle(fontSize: width*0.04,color: ColorConst.secondary,fontWeight: FontWeight.w600),),
+            ],),
+            Column(
+              children: [
+                Container(
+                  height: height*0.05,
+                  width: width*0.95,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("About us",style: TextStyle(color: ColorConst.primaryColor,fontSize: width*0.05,fontWeight: FontWeight.w600,),),
+                      Icon(Icons.arrow_forward_ios_rounded,color: ColorConst.primaryColor,)
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                      color: ColorConst.seventh,
+                      borderRadius: BorderRadius.circular(width*0.03)
+                  ),
+                ),
+                Container(
+                  height: height*0.05,
+                  width: width*0.95,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Privacy policy",style: TextStyle(color: ColorConst.primaryColor,fontSize: width*0.05,fontWeight: FontWeight.w600,),),
+                      Icon(Icons.arrow_forward_ios_rounded,color: ColorConst.primaryColor,)
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                      color: ColorConst.seventh,
+                      borderRadius: BorderRadius.circular(width*0.03)
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
