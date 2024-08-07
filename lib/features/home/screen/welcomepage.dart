aa8a9d37340e6e04bcdfe2e4d45eae5c027d1d27
musthafa <musthafakm003@gmail.com>
2024-08-07 11:48:31 +0530

    welcome page

--- lib/features/home/screen/welcomepage.dart
+++ lib/features/home/screen/welcomepage.dart
@@ -1,5 +1,4 @@
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -13,7 +12,6 @@ import 'package:pinput/pinput.dart';
 
 import '../../../main.dart';
 import '../controller/collectioncontroller.dart';
-String?CategoryId;
 
 class WelcomPage extends ConsumerStatefulWidget {
   const WelcomPage({super.key});
@@ -34,18 +32,18 @@ class _WelcomPageState extends ConsumerState<WelcomPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              CarouselSlider.builder(
-                itemCount:images.length ,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CarouselSlider.builder(
+                  itemCount:images.length ,
                   itemBuilder:(context, index, realIndex) {
-                  return Container(
-                    height: height*1,
-                    width: width*1,
-                    child: Image.asset(images[index],fit:BoxFit.fill,),
-                  );
+                    return Container(
+                      height: height*1,
+                      width: width*1,
+                      child: Image.asset(images[index],fit:BoxFit.cover,),
+                    );
                   },
                   options: CarouselOptions(
                       height: height*1,
@@ -58,92 +56,95 @@ class _WelcomPageState extends ConsumerState<WelcomPage> {
                       autoPlayAnimationDuration: Duration(seconds: 2),
                       viewportFraction:1
                   ),),
-            ],
-          ),
-          Container(
-            height: height*1,
-            width: width*1,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  stops: [0.05,0.7],
-
-                  colors: [
-                    ColorConst.thirdColor.withOpacity(0.2),
-                    ColorConst.thirdColor.withOpacity(0.8),
-                  ]
-              ),
-            ),
-            child:Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Container(
-                  height: height*0.28,
-                  width: width*0.86,
-                  margin:  EdgeInsets.only(bottom: height*0.05),
-                  decoration: BoxDecoration(
-                    color: ColorConst.primaryColor,
-                    borderRadius: BorderRadius.circular(width*0.04),
-                  ),
-                  child: Column(
-                    children: [
-                      SizedBox(height: height*0.02,),
-                      Text("Look Good, Feel Good",style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: width*0.06
-                      ),),
-                      Text("Create your individual & unique style and look amazing everyday.",style: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        color:ColorConst.twelthColor,
-                      ),textAlign: TextAlign.center,),
-                      SizedBox(height: height*0.07,),
-                      ref.watch(StreamCollection).when(
-                          data: (data) =>   Container(
-                            width: width*0.8,
-                            height: height*0.07,
-                            margin: EdgeInsets.only(left: width*0.05),
-                            child: ListView.separated(
-                              itemCount:data.length,
-                              shrinkWrap: true,
-                              scrollDirection: Axis.horizontal,
-                              itemBuilder: (context, index) {
-                                return GestureDetector(
-                                  onTap: () {
-                                    CategoryId=data[index].CategoryId;
-                                    Navigator.push(context, MaterialPageRoute(builder: (context) => HomePageView()));
-                                  },
-                                  child: Container(
-                                    alignment: Alignment.center,
-                                    child: Text(data[index].name,style: TextStyle(
-                                        color: ColorConst.primaryColor
-                                    ),),
-                                    height: height*0.03,
-                                    width: width*0.35,
-                                    decoration: BoxDecoration(
-                                        color: ColorConst.thirdColor,
-                                        borderRadius: BorderRadius.circular(width*0.02)
-
-                                    ),
-                                  ),
-                                );
-
-                              }, separatorBuilder: (context, index) {
-                              return SizedBox(width: width*0.04,);
-                            }, ),
-                          ),
-                          error: (error, stackTrace) => Text(error.toString()),
-                          loading: () =>CircularProgressIndicator())
 
 
-                    ],
-                  ),
-                )
+
+
               ],
-            ) ,
+            ),
+            Container(
+              height: height*1,
+              width: width*1,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    stops: [0.05,0.7],
 
-          ),
-        ]
+                    colors: [
+                      ColorConst.thirdColor.withOpacity(0.2),
+                      ColorConst.thirdColor.withOpacity(0.8),
+                    ]
+                ),
+              ),
+              child:Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    height: height*0.28,
+                    width: width*0.86,
+                    margin:  EdgeInsets.only(bottom: height*0.05),
+                    decoration: BoxDecoration(
+                      color: ColorConst.primaryColor,
+                      borderRadius: BorderRadius.circular(width*0.04),
+                    ),
+                    child: Column(
+                      children: [
+                        SizedBox(height: height*0.02,),
+                        Text("Look Good, Feel Good",style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: width*0.06
+                        ),),
+                        Text("Create your individual & unique style and look amazing everyday.",style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          color:ColorConst.twelthColor,
+                        ),textAlign: TextAlign.center,),
+                        SizedBox(height: height*0.07,),
+                        ref.watch(StreamCollection).when(
+                            data: (data) =>   Container(
+                              width: width*0.8,
+                              height: height*0.07,
+                              margin: EdgeInsets.only(left: width*0.05),
+                              child: ListView.separated(
+                                itemCount:data.length,
+                                shrinkWrap: true,
+                                scrollDirection: Axis.horizontal,
+                                itemBuilder: (context, index) {
+                                  return GestureDetector(
+                                    onTap: () {
+                                      Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(id: data[index].CategoryId),));
+                                    },
+                                    child: Container(
+                                      alignment: Alignment.center,
+                                      child: Text(data[index].name,style: TextStyle(
+                                          color: ColorConst.primaryColor
+                                      ),),
+                                      height: height*0.03,
+                                      width: width*0.35,
+                                      decoration: BoxDecoration(
+                                          color: ColorConst.thirdColor,
+                                          borderRadius: BorderRadius.circular(width*0.02)
+
+                                      ),
+                                    ),
+                                  );
+
+                                }, separatorBuilder: (context, index) {
+                                return SizedBox(width: width*0.04,);
+                              }, ),
+                            ),
+                            error: (error, stackTrace) => Text(error.toString()),
+                            loading: () =>CircularProgressIndicator())
+
+
+                      ],
+                    ),
+                  )
+                ],
+              ) ,
+
+            ),
+          ]
 
 
       ),
@@ -151,5 +152,3 @@ class _WelcomPageState extends ConsumerState<WelcomPage> {
     );
   }
 }
-
-
