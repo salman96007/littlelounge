96bc931b28275babaea4a44c36cd4ea881bbfd6b
musthafa <musthafakm003@gmail.com>
2024-08-08 12:37:28 +0530

    welcome page

--- lib/features/home/screen/detaileddress.dart
+++ lib/features/home/screen/detaileddress.dart
@@ -1,13 +1,20 @@
 
-  import 'package:flutter/material.dart';
+  import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:littlelounge/features/bottomnavigation/screen/bottomnavigation.dart';
+import 'package:littlelounge/features/cart/screen/cartpage.dart';
+import 'package:littlelounge/features/cart/screen/orderpage.dart';
 import 'package:littlelounge/model/productmodel.dart';
 import 'package:pinput/pinput.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 import '../../../constant/colorconstant.dart';
 import '../../../constant/imageconstant.dart';
 import '../../../main.dart';
+import '../../cart/screen/checkoutpage.dart';
 
 class DetailedDress extends ConsumerStatefulWidget {
     final ProductModel detail;
@@ -24,9 +31,17 @@ class DetailedDress extends ConsumerStatefulWidget {
         "S",
         "M",
         "L",
+        "XL",
+        "2XL",
     ];
+
     String selectedSize = '';
+    int activeIndex=0;
+
     @override
+
+
+
     Widget build(BuildContext context) {
       return Scaffold(
         bottomSheet: Row(
@@ -41,146 +56,150 @@ class DetailedDress extends ConsumerStatefulWidget {
               fontSize:width*0.04
             ),)),
           ),
-            Container(
-              height:height*0.07,
-              width:width*0.5,
-              color:ColorConst.thirdColor,
-              child:Center(child: Text("Buy now",style:TextStyle(
-                  fontWeight: FontWeight.w600,
-                  color:ColorConst.primaryColor,
-                  fontSize:width*0.04
-              ),)),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) =>CheckoutPage(),));
+              },
+              child: Container(
+                height:height*0.07,
+                width:width*0.5,
+                color:ColorConst.thirdColor,
+                child:Center(child: Text("Buy now",style:TextStyle(
+                    fontWeight: FontWeight.w600,
+                    color:ColorConst.primaryColor,
+                    fontSize:width*0.04
+                ),)),
+              ),
             )
           ],
         ),
 
         body: ListView(
           children: [
-            Stack(
-              children:[ Container(
-                  child: Image.network(widget.detail.image),
-                  width: width*1,
+            CarouselSlider.builder(
+              itemCount:widget.detail.images.length ,
+              itemBuilder:(context, index, realIndex) {
+                return Stack(
+                    children:[
+                      Container(
+                          child: Image.network(widget.detail.images[index]),
+                          width: width*1,
+                          height: height*0.45,
+                          decoration: BoxDecoration(
+                              image:DecorationImage(image:AssetImage(ImageConstant.bg),fit:BoxFit.cover)
+                          )
+                      ),
+                      Positioned(
+                          top:height*0.015,
+                          left:width*0.04,
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(context, MaterialPageRoute(builder: (context) =>HomePageView(),));
+                            },
+                            child: Icon(Icons.arrow_back),
+                          )),
+                      Positioned(
+                          top:height*0.015,
+                          right:width*0.04,
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(context, MaterialPageRoute(builder: (context) =>Orderpage(),));
+                            },
+                            child: SvgPicture.asset(SvgConstant.order),
+                          ))
+                    ]
+                );
+              },
+              options: CarouselOptions(
                   height: height*0.45,
-                  decoration: BoxDecoration(
-                      image:DecorationImage(image:AssetImage(ImageConstant.bg),fit:BoxFit.cover)
-                  )
+                  onPageChanged: (index, reason) {
+                    activeIndex=index;
+                    setState(() {
+                    });
+                  },
+                  autoPlay: false,
+                  autoPlayAnimationDuration: Duration(seconds: 2),
+                  viewportFraction:1
+              ),),
+            SizedBox(height:height*0.015),
+            Center(
+              child: AnimatedSmoothIndicator(
+                  activeIndex: activeIndex,
+                  count: widget.detail.images.length,
+                effect:
+                WormEffect(
+                  activeDotColor:ColorConst.secondary,
+                  dotColor:ColorConst.twelthColor,
+                  type: WormType.underground,
+                  dotHeight:width*0.025,
+                  dotWidth:width*0.025,
+                ),
+
               ),
-                Positioned(
-                  top:height*0.015,
-                    left:width*0.04,
-                    child: CircleAvatar(
-                  radius:width*0.066,
-                  backgroundColor:Colors.white,
-                      child:Icon(Icons.arrow_back),
-                )),
-                Positioned(
-                    top:height*0.015,
-                    right:width*0.04,
-                    child: CircleAvatar(
-                      radius:width*0.066,
-                      backgroundColor:Colors.white,
-                      child:SvgPicture.asset(SvgConstant.order),
-                    ))
-      ]
             ),
+
             Padding(
               padding:  EdgeInsets.all(width*0.05),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text("Men's Printed Pullover Hoodie",style: TextStyle(
-                          color: ColorConst.twelthColor,
-                          fontWeight: FontWeight.w400,
-                          fontSize: width*0.04
-                      ),),
-                      Text("Price",style: TextStyle(
-                          color: ColorConst.twelthColor,
-                          fontWeight: FontWeight.w400,
-                          fontSize: width*0.04
-                      ),),
-                    ],
-                  ),
+                  Text("Nike Club Fleece",style: TextStyle(
+                      color: ColorConst.secondary,
+                      fontWeight: FontWeight.w600,
+                      fontSize: width*0.06
+                  ),),
+                  Text("The Nike Throwback Pullover Hoodie is made\nfrom premium French terry fabric that blends a performance feel with",style: TextStyle(
+                      color: ColorConst.twelthColor,
+                      fontWeight: FontWeight.w400
+                  )),
+                  SizedBox(height:width*0.01,),
+                  // Text("Sale Price",style: TextStyle(
+                  //     color: ColorConst.secondary,
+                  //     fontWeight: FontWeight.w500,
+                  //     fontSize: width*0.045
+                  // ),),
+                  Text("₹500",style: GoogleFonts.archivo(
+                      fontWeight: FontWeight.w700,
+                      fontSize: width*0.065,
+
+                  ),),
+
                   SizedBox(height: height*0.005),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text("Nike Club Fleece",style: TextStyle(
-                          color: ColorConst.secondary,
-                          fontWeight: FontWeight.w600,
-                          fontSize: width*0.06
-                      ),),
-                      Row(
-                        children: [
-                          SvgPicture.asset(SvgConstant.rupees,width: width*0.05,),
-                          Text("500",style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize: width*0.05
-                          ),),
-                        ],
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: height*0.01,),
-                  Container(
-                    height: height*0.11,
-                    child: ListView.separated(itemBuilder: (context, index) {
-                      return Container(
-                        width: width*0.22,
-                        decoration: BoxDecoration(
-                            image:DecorationImage(image:AssetImage(ImageConstant.bg),fit:BoxFit.cover),
-                            borderRadius: BorderRadius.circular(width*0.03)
-                        ),
-                        child: Image.network(widget.detail.images[index]),
-                      );
-                    },
-                      shrinkWrap: true,
-                      itemCount:widget.detail.images.length,
-                      scrollDirection: Axis.horizontal, separatorBuilder: (
-                          BuildContext context, int index) {
-                        return SizedBox(
-                          width: width*0.03,
-                        );
-                      },
-                    ),
-                  ),
-                  SizedBox(height: height*0.01,),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text("Size",style: TextStyle(
                           color: ColorConst.secondary,
                           fontWeight: FontWeight.w600,
-                          fontSize: width*0.05
+                          fontSize: width*0.04
                       ),),
                       Text("Size Guide",style: TextStyle(
                           color: ColorConst.twelthColor,
                           fontWeight: FontWeight.w400,
-                          fontSize: width*0.05
+                          fontSize: width*0.04
                       ),),
                     ],
                   ),
                   SizedBox(height: height*0.01,),
                   Container(
-                    height: height*0.06,
+                    height: height*0.07,
                     child: ListView.separated(itemBuilder: (context, index) {
                       return GestureDetector(
                         onTap: () {
                           selectedSize = Size[index];
-                          setState(() {});
-                          },
-                        child: Container(
+                          setState(() {
 
+                          });
+                        },
+                        child: Container(
                             alignment: Alignment.center,
-                            width: height*0.06,
+                            width: width*0.155,
                             decoration: BoxDecoration(
-                                color: selectedSize== Size[index]? ColorConst.seventh:ColorConst.sixth,
+                                color: selectedSize== Size[index]? ColorConst.thirdColor.withOpacity(0.15):ColorConst.eleventh,
                                 borderRadius: BorderRadius.circular(width*0.03)
                             ),
                             child: Text(Size[index],style: TextStyle(
-                                color:selectedSize== Size[index]? ColorConst.primaryColor:ColorConst.secondary,
+                                color: ColorConst.secondary,
                                 fontWeight: FontWeight.w600,
                                 fontSize: width*0.05
                             ),)
@@ -188,7 +207,7 @@ class DetailedDress extends ConsumerStatefulWidget {
                       );
                     },
                       shrinkWrap: true,
-                      itemCount: 3,
+                      itemCount: 5,
                       scrollDirection: Axis.horizontal, separatorBuilder: (
                           BuildContext context, int index) {
                         return SizedBox(
@@ -198,25 +217,7 @@ class DetailedDress extends ConsumerStatefulWidget {
                     ),
                   ),
                   SizedBox(height: height*0.02,),
-                  Text("Description",style: TextStyle(
-                      color: ColorConst.secondary,
-                      fontSize: width*0.05,
-                      fontWeight: FontWeight.w600
-                  ),),
-                  SizedBox(height: height*0.02,),
-                  RichText(text: TextSpan(
-                     text: "The Nike Throwback Pullover Hoodie is made\nfrom premium French terry fabric that blends a performance feel with",style: TextStyle(
-                     color: ColorConst.twelthColor,
-                     fontWeight: FontWeight.w400
-                   ),children: [
-                     TextSpan(text: " Read More..",style: TextStyle(
-                       color: ColorConst.secondary,
-                       fontWeight: FontWeight.w600,
-                       fontSize: width*0.045
-                     ))
-                   ]
-                   ),textAlign: TextAlign.start,),
-                  SizedBox(height: height*0.02,),
+
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
