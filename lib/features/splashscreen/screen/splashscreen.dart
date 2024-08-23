a55ddb8edd938eb88fdef8867bc1dcdfe4d425ee
salman <salmanmonusalman66@gmail.com>
2024-08-23 11:58:03 +0530

    shered preferences added

--- lib/features/splashscreen/screen/splashscreen.dart
+++ lib/features/splashscreen/screen/splashscreen.dart
@@ -4,9 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:littlelounge/constant/colorconstant.dart';
 import 'package:littlelounge/constant/imageconstant.dart';
 import 'package:littlelounge/features/auth/screen/createaccount.dart';
-import 'package:littlelounge/features/home/screen/welcomepage.dart';
 import 'package:littlelounge/main.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreen extends ConsumerStatefulWidget {
     const SplashScreen({super.key});
@@ -16,14 +14,13 @@ class SplashScreen extends ConsumerStatefulWidget {
   }
 
   class _SplashScreenState extends ConsumerState<SplashScreen> {
-    bool login=false;
-    getData() async {
-      SharedPreferences prefs=await SharedPreferences.getInstance();
-      login=prefs.getBool("login")??false;
-      Future.delayed(Duration(
-          seconds: 3
-      )).then((value) => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => login?WelcomPage():CreatAccount(),)));
-    }
+  @override
+  void initState() {
+    Future.delayed(Duration(
+      seconds: 3,
+    )).then((value) => Navigator.pushAndRemoveUntil(context,MaterialPageRoute(builder: (context) =>CreatAccount(),),(route) => false,));
+    super.initState();
+  }
     @override
     Widget build(BuildContext context) {
       return Scaffold(
